29849f3eb12170e8091e3b5428913359bb81ddcd
Andy Erickson <andy.erickson@digikey.com>
2023-11-24 17:26:36 -0600

    adding support for firebase backend

--- src/index.js
+++ src/index.js
@@ -14,3 +14,51 @@ const client = new Client({
 eventHandler(client);
 
 client.login(process.env.TOKEN);
+
+
+
+
+
+
+const { initializeApp } = require('firebase/app');
+const {
+  getFirestore,
+  collection,
+  getDocs
+} = require('firebase/firestore');
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+};
+
+// init firebase app
+initializeApp(firebaseConfig);
+
+// init services
+const db = getFirestore();
+
+// collection ref
+const colectionReference = collection(db, 'characters');
+
+// get collection data
+getDocs(colectionReference)
+  .then((snapshot) => {
+    let chars = [];
+
+    snapshot.docs.forEach((doc) => {
+      chars.push({
+        id: doc.id,
+        ...doc.data()
+      });
+    });
+
+    console.log(chars);
+  })
+  .catch((error) => console.error(error.message));
